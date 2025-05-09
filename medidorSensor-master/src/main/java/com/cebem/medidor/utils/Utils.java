e1032ddb1d81c8f317763f8a8ac4a6977c6b93b6
Xandre-01 <xandre.glezm@gmail.com>
2025-05-09 12:16:02 +0200

    Delete medidorSensor-master directory

--- medidorSensor-master/src/main/java/com/cebem/medidor/utils/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.cebem.medidor.utils;
-
-public class Utils {
-    public static boolean isPalindrome(String text){
-        String inverse = new StringBuilder(text).reverse().toString();
-        return text.equalsIgnoreCase(inverse);
-    }
-}
